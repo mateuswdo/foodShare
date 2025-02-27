5bbd626a5a46f26001265047af355e87ddac15b0
mateus <mateusweslley14@gmail.com>
2025-02-26 23:38:47 -0300

    add logout functionality and update Profile screen layout

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,10 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, Alert } from "react-native";
 import { styles } from "./styles";
+import { useAuth } from "@/hooks/useAuth";
+import { AppNavigatorRoutesProps } from "@/routes/protected.routes";
+import { useNavigation } from "@react-navigation/native";
+import { Button } from "@/components/Button";
 
 export function Profile() {
+  const { logout, user } = useAuth();
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  const handleLogout = async () => {
+    Alert.alert("Confirmação", "Deseja realmente sair?", [
+      { text: "Cancelar", style: "cancel" },
+      {
+        text: "Sair",
+        style: "destructive",
+        onPress: async () => {
+          logout();
+        },
+      },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Doar</Text>
+      <Button title="Sair" onPress={handleLogout} />
+      <Text>Profile</Text>
     </View>
   );
 }
