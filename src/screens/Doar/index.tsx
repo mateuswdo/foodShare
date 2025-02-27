dad208c03221c07e205ce1e7452d3b51f9db244d
mateus <mateusweslley14@gmail.com>
2025-02-26 22:45:02 -0300

    add Doar screen with basic layout and styles

---
+++ src/screens/Doar/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+export function Doar() {
+  return (
+    <View style={styles.container}>
+      <Text>Doar</Text>
+    </View>
+  );
+}
