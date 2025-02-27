17bcd5bb5dcc0d0994dd8ddc7c3312c189d2579b
mateus <mateusweslley14@gmail.com>
2025-02-27 00:03:19 -0300

    ``` No code changes made ```

--- src/screens/Scan/index.tsx
+++ src/screens/Scan/index.tsx
@@ -1,32 +1,32 @@
-import { StyleSheet, Modal, Text, View, Alert } from "react-native";
+import { Modal, Text, View, Alert } from "react-native";
 import { Button } from "@/components/Button";
-import { Styles } from "./style"
 import { CameraView, useCameraPermissions } from "expo-camera";
 import { useState, useRef } from "react";
 
-export function Scan() {
-  const [modalIsVisible, setModalIsVisible] = useState(false)
-  const [permission, requestPermission] = useCameraPermissions()
+import { styles } from "./style";
 
-  const qrCodeLock = useRef(false)
+export function Scan() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const [permission, requestPermission] = useCameraPermissions();
+
+  const qrCodeLock = useRef(false);
 
   async function handleOpenCamera() {
     try {
-
-      const { granted } = await requestPermission()
+      const { granted } = await requestPermission();
       if (!granted) {
-        return Alert.alert("Camera", "Você precisa habilitar o uso da camera")
+        return Alert.alert("Camera", "Você precisa habilitar o uso da camera");
       }
-      setModalIsVisible(true)
-      qrCodeLock.current = false
+      setModalIsVisible(true);
+      qrCodeLock.current = false;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
   function handleQRCodeRead(data: string) {
-    setModalIsVisible(false)
-    Alert.alert("QR code", data)
+    setModalIsVisible(false);
+    Alert.alert("QR code", data);
   }
   return (
     <View style={styles.container}>
@@ -34,55 +34,27 @@ export function Scan() {
         <Text style={styles.title}>Vamos verificar seu código!</Text>
       </View>
 
-      <View style={styles.inputsContainer}>
-        <Button title="Ler QR code" onPress={handleOpenCamera} />
-      </View>
-
+      <Button icon="camera" title="Ler QR code" onPress={handleOpenCamera} />
 
       <Modal visible={modalIsVisible} style={{ flex: 1 }}>
-      <CameraView
-      style={{ flex: 1 }}
-      facing="back"
-      onBarcodeScanned={({ data }) => {
-        if (data && !qrCodeLock.current) {
-          qrCodeLock.current = true; 
-          setTimeout(() => {
-            handleQRCodeRead(data);
-            qrCodeLock.current = false;
-          }, 500); 
-        }
-      }}
-    />
-
+        <CameraView
+          style={{ flex: 1 }}
+          facing="back"
+          onBarcodeScanned={({ data }) => {
+            if (data && !qrCodeLock.current) {
+              qrCodeLock.current = true;
+              setTimeout(() => {
+                handleQRCodeRead(data);
+                qrCodeLock.current = false;
+              }, 500);
+            }
+          }}
+        />
 
         <View>
-          <Button title="Cancelar"
-            onPress={() => setModalIsVisible(false)} />
+          <Button title="Cancelar" onPress={() => setModalIsVisible(false)} />
         </View>
       </Modal>
-
-
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  footer: {
-    position: "absolute",
-    bottom: 32,
-    left: 32,
-    right: 32,
-  },
-  title: {
-    color: "black",
-  },
-  inputsContainer: {
-
-  },
-})
