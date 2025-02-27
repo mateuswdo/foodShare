20a783c0092597d3f156a36cae0a7ce9bd5d8fb2
mateus <mateusweslley14@gmail.com>
2025-02-26 23:39:03 -0300

    add protected routes with bottom tab navigation for authenticated users

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { FormProvider, useForm } from "react-hook-form";
 
 import PublicRoutes from "./public.routes";
+import ProtectedRoutes from "./protected.routes";
 
 import { useAuth } from "@/hooks/useAuth";
 
@@ -10,10 +11,12 @@ export default function Routes() {
   const { user, isLoadingUserStorageData } = useAuth();
   const methods = useForm();
 
+  console.log({ user });
+
   return (
     <NavigationContainer>
       <FormProvider {...methods}>
-        <PublicRoutes />
+        {user.id ? <ProtectedRoutes /> : <PublicRoutes />}
       </FormProvider>
     </NavigationContainer>
   );
---
+++ src/routes/protected.routes.tsx
@@ -0,0 +1,59 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+
+import { Scan } from "@/screens/Scan";
+import { Profile } from "@/screens/Profile";
+import { Donation } from "@/screens/Donation";
+import { Dashboard } from "@/screens/Dashboard";
+
+type ProtectedRoutes = {
+  dashboard: undefined;
+  donation: undefined;
+  profile: undefined;
+  scan: undefined;
+};
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<ProtectedRoutes>;
+
+const Tab = createBottomTabNavigator<ProtectedRoutes>();
+
+export default function ProtectedRoutes() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen
+        name="dashboard"
+        component={Dashboard}
+        options={{
+          headerTitle: "",
+        }}
+      />
+
+      <Tab.Screen
+        name="scan"
+        component={Scan}
+        options={{
+          headerTitle: "",
+        }}
+      />
+
+      <Tab.Screen
+        name="donation"
+        component={Donation}
+        options={{
+          headerTitle: "",
+        }}
+      />
+      <Tab.Screen
+        name="profile"
+        component={Profile}
+        options={{
+          headerTitle: "",
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
